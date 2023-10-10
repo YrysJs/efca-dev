0982a66714e734dbe307dbde5117cc6aebae3e40
YrysJpeg <iriskeldicloud@gmail.com>
2023-10-10 23:03:00 +0600

    test3

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,13 +1,13 @@
-/** @type {import('next').NextConfig} */
-const path = require('path')
+// /** @type {import('next').NextConfig} */
+// const path = require('path')
 
-const nextConfig = {
-  i18n: {
-    locales: ['ru', 'kz', 'en'],
-    defaultLocale: 'ru',
-    // localePath: path.resolve('./public/locales'),
-    localeStructure: '{{lng}}/{{ns}}',
-  },
-}
+// const nextConfig = {
+//   i18n: {
+//     locales: ['ru', 'kz', 'en'],
+//     defaultLocale: 'ru',
+//     // localePath: path.resolve('./public/locales'),
+//     localeStructure: '{{lng}}/{{ns}}',
+//   },
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
